59bb2d4221cbf3f644d7a81993a8002bc816a1e8
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2017-08-16 12:08:04 +0200

    Client works

---
+++ TCP_Client/TCP_Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TcpClient client = new TcpClient();
+            Console.WriteLine("Connecting...");
+
+            client.Connect("localhost", 20000);
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            writer.WriteLine("hello server");
+            Console.WriteLine(reader.ReadLine());
+
+            Console.ReadLine();
+
+            client.Close();
+
+        }
+    }
+}
---
+++ TCP_Server/TCP_Server/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 20000);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine(" >> Server Started");
+
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> Client No: " + counter + " Started!");
+                HandleClient client = new HandleClient();
+                client.StartClient(clientSocket, counter);
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+
+        public class HandleClient
+        {
+            private TcpClient clientSocket;
+            private string clNo;
+
+            internal void StartClient(TcpClient inClientSocket, int clientNo)
+            {
+                this.clientSocket = inClientSocket;
+                clNo = clientNo.ToString();
+                Thread newThread = new Thread(ClientHandler);
+                newThread.Start();
+
+            }
+
+            internal void ClientHandler()
+            {
+                while (true)
+
+                {
+                    
+
+                    IPEndPoint remoteIpEndPoint = clientSocket.Client.RemoteEndPoint as IPEndPoint;
+                    IPEndPoint localIpEndPoint = clientSocket.Client.LocalEndPoint as IPEndPoint;
+
+                    NetworkStream stream = new NetworkStream(clientSocket.Client);
+                    StreamReader reader = new StreamReader(stream);
+                    StreamWriter writer = new StreamWriter(stream);
+                    writer.AutoFlush = true;
+
+                    if (remoteIpEndPoint != null)
+                    {
+                        Console.WriteLine("I am connected to " + remoteIpEndPoint.Address + " on port number " +
+                                          remoteIpEndPoint.Port);
+                    }
+
+                    if (localIpEndPoint != null)
+                    {
+                        Console.WriteLine("My local IpAddress is :" + localIpEndPoint.Address +
+                                          " I am connected on port number " + localIpEndPoint.Port);
+                    }
+
+                    if (reader.ReadLine().ToLower() == "hello server")
+                    {
+                        writer.WriteLine("Hello, Client!");
+                    }
+
+
+                    while (clientSocket.Client.Connected)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
